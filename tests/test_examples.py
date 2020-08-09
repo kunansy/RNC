c7c296712b468be18bdc29dc911208b196cdbd6b
Kirill <anonymouse_name@mail.ru>
2020-08-09 15:00:14 +0300

    Tests updated

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -15,8 +15,26 @@ class TemplateTestExamples:
         copy.txt = new_txt
         assert copy.txt == new_txt
 
-    def test_src(self):
+    def test_text_setter_with_notstr_type(self):
+        copy = self.ex.copy()
+        new_txt = list('new ex')
+        copy.txt = new_txt
+        assert copy.txt == new_txt
+
+    def test_src_getter(self):
         assert isinstance(self.ex.src, str) and self.ex.src
+
+    def test_src_setter(self):
+        copy = self.ex.copy()
+        new_src = 'test_source'
+        copy.src = new_src
+        assert copy.src == new_src
+
+    def test_src_setter_with_notstr_type(self):
+        copy = self.ex.copy()
+        new_src = list('test_source')
+        copy.src = new_src
+        assert copy.src == new_src
 
     def test_ambiguation_getter(self):
         amb = self.ex.ambiguation
@@ -25,6 +43,12 @@ class TemplateTestExamples:
     def test_ambiguation_setter(self):
         copy = self.ex.copy()
         new_amb = 'new_amb'
+        copy.ambiguation = new_amb
+        assert copy.ambiguation == new_amb
+
+    def test_ambiguation_setter_with_nostr_type(self):
+        copy = self.ex.copy()
+        new_amb = list('new_amb')
         copy.ambiguation = new_amb
         assert copy.ambiguation == new_amb
 
@@ -70,8 +94,23 @@ class TemplateTestExamples:
         with pytest.raises(AttributeError):
             del self.ex.txt
 
-    def test_normal_copy(self):
+    def test_copy(self):
         assert self.ex == self.ex.copy()
+
+    def test_contains(self):
+        item = self.ex.txt[:5]
+        assert item in self.ex
+
+    def test_contains_with_notstr_type(self):
+        copy = self.ex.copy()
+        copy.txt = ['1', 2, 3, 4, '5']
+        assert '1' in copy and 2 in copy
+
+    def test_equal(self):
+        lhs = rhs = self.ex.copy()
+        lhs.txt = list('test lhs')
+        rhs.txt = lhs.txt
+        assert lhs == rhs
 
 
 class TestMainExample(TemplateTestExamples):
@@ -89,6 +128,46 @@ class TestKwicExample(TemplateTestExamples):
         with pytest.raises(NotImplementedError):
             self.ex.txt = 'sth'
 
+    def test_text_setter_with_notstr_type(self):
+        with pytest.raises(NotImplementedError):
+            self.ex.txt = 123
+
+    def test_left_setter(self):
+        copy = self.ex.copy()
+        new_left = 'new left'
+        copy.left = new_left
+        assert copy.left == new_left
+
+    def test_left_setter_with_notstr_type(self):
+        copy = self.ex.copy()
+        new_left = list('new left')
+        copy.left = new_left
+        assert copy.left == new_left
+
+    def test_center_setter(self):
+        copy = self.ex.copy()
+        new_center = 'new center'
+        copy.center = new_center
+        assert copy.center == new_center
+
+    def test_center_setter_with_notstr_type(self):
+        copy = self.ex.copy()
+        new_center = list('new center')
+        copy.center = new_center
+        assert copy.center == new_center
+
+    def test_right_setter(self):
+        copy = self.ex.copy()
+        new_right = 'new right'
+        copy.right = new_right
+        assert copy.right == new_right
+
+    def test_right_setter_with_notstr_type(self):
+        copy = self.ex.copy()
+        new_right = list('new right')
+        copy.right = new_right
+        assert copy.right == new_right
+
     def test_ambiguation_getter(self):
         with pytest.raises(NotImplementedError):
             amb = self.ex.ambiguation
@@ -96,6 +175,10 @@ class TestKwicExample(TemplateTestExamples):
     def test_ambiguation_setter(self):
         with pytest.raises(AttributeError):
             self.ex.ambiguation = 'sth'
+
+    def test_ambiguation_setter_with_nostr_type(self):
+        with pytest.raises(AttributeError):
+            self.ex.ambiguation = 123
 
     def test_columns(self):
         columns = self.ex.columns
@@ -121,6 +204,18 @@ class TestKwicExample(TemplateTestExamples):
 
         assert copy.left == copy.center == copy.right == expected_txt
 
+    def test_contains_with_notstr_type(self):
+        copy = self.ex.copy()
+        copy.left = [1, 2, 3]
+        copy.center = '1'
+        copy.right = 12
+        assert '12' in copy
+
+    def test_equal(self):
+        lhs = rhs = self.ex.copy()
+        lhs.left = rhs.left = ''
+        assert lhs == rhs
+
 
 class TestParallelExample(TemplateTestExamples):
     corpus_res = corp.ParallelCorpus(
@@ -136,13 +231,106 @@ class TestParallelExample(TemplateTestExamples):
         with pytest.raises(NotImplementedError):
             self.ex.txt = {}
 
+    def test_text_setter_with_notstr_type(self):
+        with pytest.raises(NotImplementedError):
+            self.ex.txt = 12
+
     def test_data(self):
         data = self.ex.data
         assert isinstance(data, dict) and len(data) is 5
 
-    def test_iadd(self):
-        # TODO
-        pass
+    def test_iadd_with_nochanges(self):
+        copy = self.ex.copy()
+        new_ex = expl.ParallelExample()
+        copy += new_ex
+        assert copy == self.ex
+
+    def test_iadd_with_extend_texts(self):
+        txt = {
+            'ru': 'текст1',
+            'en': 'text1'
+        }
+        lhs = expl.ParallelExample(txt, 'old_src', 'not amb', ['test'])
+
+        update = {
+            'ru': 'и текст2',
+            'en': 'and text2'
+        }
+        rhs = expl.ParallelExample(update, 'old src | new src', 'amb', ['test1'])
+        lhs += rhs
+        assert lhs.ru == 'текст1 и текст2'
+        assert lhs.en == 'text1 and text2'
+        assert lhs.src == 'old src | new src'
+        assert lhs.ambiguation == 'amb'
+        assert lhs.found_wordforms == ['test', 'test1']
+
+    def test_iadd_with_new_lang(self):
+        txt = {
+            'ru': 'текст1',
+            'en': 'text1'
+        }
+        lhs = expl.ParallelExample(txt, 'old src | new src', 'amb', ['test'])
+
+        update = {
+            'fr': 'и текст2',
+            'ch': 'and text2'
+        }
+        rhs = expl.ParallelExample(update, 'new src', 'not amb', [])
+        lhs += rhs
+
+        assert lhs.ru == 'текст1'
+        assert lhs.en == 'text1'
+        assert lhs.fr == 'и текст2'
+        assert lhs.ch == 'and text2'
+        assert lhs.src == 'old src | new src'
+        assert lhs.ambiguation == 'amb'
+        assert lhs.found_wordforms == ['test']
+
+    def test_iadd_with_new_lang_and_extend_texts(self):
+        txt = {
+            'ru': 'текст1',
+            'en': 'text1'
+        }
+        lhs = expl.ParallelExample(txt, 'old src', 'not amb', ['test'])
+
+        update = {
+            'ru': 'и текст2',
+            'en': 'and text2',
+            'fr': 'и текст3',
+            'ch': 'and text3'
+        }
+        rhs = expl.ParallelExample(update, '', '', ['text1'])
+        lhs += rhs
+
+        assert lhs.ru == 'текст1 и текст2'
+        assert lhs.en == 'text1 and text2'
+        assert lhs.fr == 'и текст3'
+        assert lhs.ch == 'and text3'
+        assert lhs.src == 'old src'
+        assert lhs.ambiguation == 'not amb'
+        assert lhs.found_wordforms == ['test', 'text1']
+
+    def test_iadd_with_update_ambiguation_from_null(self):
+        txt = {
+            'ru': 'текст1',
+            'en': 'text1'
+        }
+        lhs = expl.ParallelExample(txt, 'old src', '', [])
+
+        update = {
+            'fr': 'и текст2',
+            'ch': 'and text2'
+        }
+        rhs = expl.ParallelExample(update, 'new src', 'not amb', [])
+        lhs += rhs
+
+        assert lhs.ru == 'текст1'
+        assert lhs.en == 'text1'
+        assert lhs.fr == 'и текст2'
+        assert lhs.ch == 'and text2'
+        assert lhs.src == 'old src'
+        assert lhs.ambiguation == 'not amb'
+        assert lhs.found_wordforms == []
 
     def test_columns(self):
         columns = self.ex.columns
@@ -171,6 +359,12 @@ class TestParallelExample(TemplateTestExamples):
         copy['ru'] = new_txt
         assert copy.ru == new_txt
 
+    def test_setitem_with_notstr_type(self):
+        copy = self.ex.copy()
+        new_txt = list('new txt')
+        copy['ru'] = new_txt
+        assert copy.ru == new_txt
+
     def test_sort(self):
         copy = self.ex.copy()
         copy.sort()
@@ -192,3 +386,18 @@ class TestParallelExample(TemplateTestExamples):
 
     def test_equality(self):
         assert self.ex['ru'] == self.ex.ru
+
+    def test_equal(self):
+        lhs = rhs = self.ex.copy()
+        lhs['fr'] = rhs['fr'] = ''
+        assert lhs == rhs
+
+    def test_contains(self):
+        text = self.ex.ru[:5]
+        assert text in self.ex
+
+    def test_contains_with_notstr_type(self):
+        copy = self.ex.copy()
+        new_txt = ['1', 2, '3', '5']
+        copy['ru'] = new_txt
+        assert '1' in copy and '3' in copy
