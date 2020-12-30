a535da368adcd0612b76e7fa500c8ff9e2e06c85
kunansy <kolobov.kirill@list.ru>
2020-12-30 11:27:26 +0300

    Tests for paper regional corpus moved

---
+++ tests/corpora/test_paper_regional_corpus.py
@@ -0,0 +1,39 @@
+from time import sleep
+
+import pytest
+
+import rnc.corpora as rnc
+from tests.corpora.template import TemplateCorpusTest
+
+
+class TestPaperRegionalCorpus(TemplateCorpusTest):
+    corp_type = rnc.PaperRegionalCorpus
+
+    corp_normal_obj = corp_type('ты', 1, dpp=5, spd=1)
+    corp_kwic_obj = corp_type('ты', 1, dpp=5, spd=1, out='kwic')
+
+    corp_normal_obj.request_examples()
+    sleep(5)
+    corp_kwic_obj.request_examples()
+    sleep(5)
+
+    def test_full_query_with_lexform(self):
+        corp = self.corp_type('Владимир Путин', 1, text='lexform')
+        corp.request_examples()
+
+        assert len(corp) > 1
+        sleep(5)
+
+    def test_mycorp(self):
+        corp = self.corp_type(
+            'ты', 1,
+            mycorp="JSONeyJkb2NfaV9sZV9zdGFydF95ZWFyIjogWyIyMDEwIl19"
+        )
+        corp.request_examples()
+
+        assert len(corp) >= 1
+        sleep(5)
+
+    def test_open_graphic(self):
+        with pytest.raises(RuntimeError):
+            self.corp_normal_obj.open_graphic()
